fd3c813b522436d5af0eaa37165ea4f9af887b2a
mjbahmani <mohamadjavad.bahmani@gmail.com>
2021-05-06 17:03:45 +0300

    Format code by black

--- chat.py
+++ chat.py
@@ -6,9 +6,9 @@ import torch
 from model import NeuralNet
 from nltk_utils import bag_of_words, tokenize
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-with open('intents.json', 'r') as json_data:
+with open("intents.json", "r") as json_data:
     intents = json.load(json_data)
 
 FILE = "data.pth"
@@ -17,8 +17,8 @@ data = torch.load(FILE)
 input_size = data["input_size"]
 hidden_size = data["hidden_size"]
 output_size = data["output_size"]
-all_words = data['all_words']
-tags = data['tags']
+all_words = data["all_words"]
+tags = data["tags"]
 model_state = data["model_state"]
 
 model = NeuralNet(input_size, hidden_size, output_size).to(device)
@@ -46,7 +46,7 @@ while True:
     probs = torch.softmax(output, dim=1)
     prob = probs[0][predicted.item()]
     if prob.item() > 0.75:
-        for intent in intents['intents']:
+        for intent in intents["intents"]:
             if tag == intent["tag"]:
                 print(f"{bot_name}: {random.choice(intent['responses'])}")
     else:
